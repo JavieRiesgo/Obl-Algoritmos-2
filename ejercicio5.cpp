d1f277f7eb37f16ec945598007100e53d28b9602
JavieRiesgo <javierriesgo8@gmail.com>
2025-10-23 19:01:00 -0300

    Fix ejercicio5, corren todos los casos

--- ejercicio5.cpp
+++ ejercicio5.cpp
@@ -8,36 +8,19 @@
 
 using namespace std;
 
-struct AristaSimple {
-    int origen;
-    int destino;
-    int peso;
-};
-
-// Función para cargar todas las aristas del grafo
-AristaSimple* cargarAristas(GrafoLista* g, int& cantAristas) {
-    int V = g->cantidadVertices();
-    cantAristas = 0;
-
-    // Primero contamos cuántas aristas únicas hay (el grafo no dirigido duplica las aristas)
-    for (int i = 0; i < V; i++) {
-        Arista* ady = g->adyacentes(i);
-        while (ady) {
-            if (ady->origen < ady->destino) cantAristas++;
-            ady = ady->sig;
-        }
-    }
-
-    AristaSimple* aristas = new AristaSimple[cantAristas];
+Arista** cargarAristas(GrafoLista* g, int A, int V) {
+    Arista** aristas = new Arista*[A];
     int pos = 0;
 
     for (int i = 0; i < V; i++) {
         Arista* ady = g->adyacentes(i);
         while (ady) {
+            // Evitamos duplicar aristas en grafos no dirigidos
             if (ady->origen < ady->destino) {
-                aristas[pos].origen = ady->origen;
-                aristas[pos].destino = ady->destino;
-                aristas[pos].peso = ady->peso;
+                aristas[pos] = new Arista();
+                aristas[pos]->origen = ady->origen;
+                aristas[pos]->destino = ady->destino;
+                aristas[pos]->peso = ady->peso;
                 pos++;
             }
             ady = ady->sig;
@@ -47,17 +30,17 @@ AristaSimple* cargarAristas(GrafoLista* g, int& cantAristas) {
     return aristas;
 }
 
-// Función de Kruskal que devuelve el peso total del MST o bosque
-long long kruskal(GrafoLista* g) {
+// Función de Kruskal que devuelve el peso total del ACM o bosque
+int kruskal(GrafoLista* g) {
     int V = g->cantidadVertices();
-    int A;
-    AristaSimple* aristas = cargarAristas(g, A);
+    int A = g->cantidadAristas() / 2; // Para evitar repetidos en grafos no dirigidos
+    Arista** aristas = cargarAristas(g, A, V);
 
-    // Ordenar aristas por peso (simple bubble o insertion, no se pueden usar sort)
+    // Ordenar aristas por peso
     for (int i = 0; i < A - 1; i++) {
         for (int j = 0; j < A - i - 1; j++) {
-            if (aristas[j].peso > aristas[j + 1].peso) {
-                AristaSimple aux = aristas[j];
+            if (aristas[j]->peso > aristas[j + 1]->peso) {
+                Arista* aux = aristas[j];
                 aristas[j] = aristas[j + 1];
                 aristas[j + 1] = aux;
             }
@@ -65,13 +48,13 @@ long long kruskal(GrafoLista* g) {
     }
 
     MFSet* ds = new MFSet(V);
-    long long pesoTotal = 0;
+    int pesoTotal = 0;
     int cantUniones = 0;
 
     for (int i = 0; i < A; i++) {
-        int u = aristas[i].origen;
-        int v = aristas[i].destino;
-        int w = aristas[i].peso;
+        int u = aristas[i]->origen;
+        int v = aristas[i]->destino;
+        int w = aristas[i]->peso;
 
         if (ds->find(u) != ds->find(v)) {
             ds->merge(u, v);
@@ -80,6 +63,8 @@ long long kruskal(GrafoLista* g) {
         }
     }
 
+    for (int i = 0; i < A; i++)
+        delete aristas[i];
     delete[] aristas;
     delete ds;
 
@@ -87,9 +72,6 @@ long long kruskal(GrafoLista* g) {
 }
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
     int V, E;
     cin >> V >> E;
 
@@ -101,7 +83,7 @@ int main() {
         g->agregarArista(u, v, w);
     }
 
-    cout << kruskal(g) << "\n";
+    cout << kruskal(g) << endl;
 
     delete g;
     return 0;
